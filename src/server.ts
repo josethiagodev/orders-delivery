a0826a46c9bc896f1a588b917f3aacfc479150c2
José Thiago <josethiagowd@gmail.com>
2026-05-13 01:21:44 -0300

    Cadastrando Usuário

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express from "express";
+import express, { Request, Response } from "express";
 import { router } from "./routes.js";
 
 const app = express();
@@ -7,6 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+
+app.use( (error: Error, _: Request, res: Response) => {
+  if(error instanceof Error) {
+    return res.status(400).json({
+      error: error.message
+    });
+  }
+
+  return res.status(500).json({
+    error: "Erro no servidor interno!"
+  })
+})
 
 const PORT = process.env.PORT! || 3333;
 
