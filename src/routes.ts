7b6174de202bd3ef5dfc24d71537caae137d3def
José Thiago <josethiagowd@gmail.com>
2026-05-15 20:28:08 -0300

    Detalhes do usuário logado (Rota com método GET > /me)

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 // Controller ()
 import { CreateUserController } from "./controllers/user/CreateUserController.js";
 import { AuthUserController } from "./controllers/user/AuthUserController.js";
+import { DetailUserController } from "./controllers/user/DetailUserController.js";
 
 // Validação (EXPRESS + ZOD)
 import { validateSchema } from "./middlewares/validateSchema.js";
@@ -11,16 +12,22 @@ import { createUserSchema, authUserSchema } from "./schemas/userSchema.js";
 
 const router = Router();
 
-// ROTA => Criar Usuário
+// ROTA => Criar Usuário (POST)
 router.post(
     "/users", 
     validateSchema(createUserSchema), new CreateUserController().handle
 );
 
-// ROTA => Fazer login por sessão
+// ROTA => Fazer login por sessão (POST)
 router.post(
     "/session", 
     validateSchema(authUserSchema), new AuthUserController().handle
+);
+
+// ROTA => Buscar detalhes do usuário (GET)
+router.get(
+    "/me",
+    new DetailUserController().handle
 );
 
 export { router };
