dede1137a168f9462870ba4ff2f544366cf047b1
José Thiago <josethiagowd@gmail.com>
2026-05-15 01:26:58 -0300

    Gerando hash e realizando login com JWT (Json Web Token)

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,3 +1,8 @@
+import { compare } from "bcrypt"
+import prismaClient from "../../prisma/index"
+
+import jwt from 'jsonwebtoken';
+
 interface AuthUserServiceProps {
     email: string;
     password: string;
@@ -5,8 +10,45 @@ interface AuthUserServiceProps {
 
 export class AuthUserService {
     async execute({ email, password }: AuthUserServiceProps) {
-        console.log({  email, password })
+        
+        // Verificar se usuário não existir
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
 
-        return "Usuário LOGADO"
+        if(!user) {
+            throw new Error("Email/Senha é obrigatório")
+        }
+
+
+        // Verificar se a senha está correta
+        const passwordMatch = await compare(password, user.password)
+
+        if(!passwordMatch) {
+            throw new Error("Email/Senha é obrigatório")
+        }
+
+
+        // Gerar Hash (JWT = Json Web Token)
+        const token = jwt.sign({
+            name: user.name,
+            email: user.email
+        }, 
+        process.env.JWT_SECRET as string, {
+            subject: user.id,
+            expiresIn: "1h"
+        })
+
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: token
+        }
+
     }
 }
