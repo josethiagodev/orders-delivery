1e602049aae0e83369d17fab6ed3facbf29007c0
José Thiago <josethiagowd@gmail.com>
2026-05-14 21:47:20 -0300

    Criptografando Senha (BcryptJS) + Melhorando Dados de retorno (CreateUserController.ts)

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -13,7 +13,7 @@ class CreateUserController {
             password: password,
         })
 
-        res.json({ message: user })
+        res.json(user);
     }
 }
 
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -28,10 +28,17 @@ class CreateUserService {
                 name: name,
                 email: email,
                 password: passwordHash,
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                createdAt: true
             }
         })
 
-        return user.name
+        return user;
 
     }
 }
